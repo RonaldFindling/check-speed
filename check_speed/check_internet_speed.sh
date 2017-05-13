40dff753088478aa4a03faccdec7e3bbdc68d482
Ronald Findling <ronald.findling@gmail.com>
2017-05-13 15:57:45 +0200

    Add capture option for ping.

--- check_speed/check_internet_speed.sh
+++ check_speed/check_internet_speed.sh
@@ -8,5 +8,7 @@ time=`date '+%Y.%m.%d %X'`
 speedtestResult=`speedtest-cli ` #| grep -E "Download: |Upload: "`
 download=`echo $speedtestResult | grep -E "Download: " | sed 's/.*Download: \([0-9]\.[0-9][0-9]*\).*/\1/' `
 upload=`echo $speedtestResult | grep -E "Upload: " | sed 's/.*Upload: \([0-9]\.[0-9][0-9]*\).*/\1/' `
+ping=`echo $speedtestResult | grep -E "Hosted by " | sed 's/.*Hosted by \S* (\w*) \[[0-9]*[.]*[0-9]* km\]: \([0-9]*[.]*[0-9]*\) ms.*/\1/'`
+
 printf "${time},${download},${upload}\n"
 
